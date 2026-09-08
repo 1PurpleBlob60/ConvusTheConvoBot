2d22cc25fd607dbc531334764b0f1bff9f9283d7
1PurpleBlob60 <mahe1@skole.trondheim.kommune.no>
2026-09-08 14:16:52 +0000

    moves the responses to responses file (broken)

--- SimpleConvus.py
+++ SimpleConvus.py
@@ -1,13 +1,10 @@
 import random 
+import responses as resp
 
-resp1 = ["Great", "Good to hear that", "Awesome!"]
-resp2 = ["Good, you?", "I'm good, you?"]
-resp3 = ["Hi There!", "Hi!", "Hello"]
-resp4 = ["What do you like?", "What is you favourite food?"]
-resp_followup = ["That's great!", "Wow", "Awesome"]
-resp_joke = ["Why can't you tell a joke to an egg? It might crack up!", "What do you call a magic dog? A labracadabrador!"]
 
-waiting_for_answer = False
+
+waiting_for_answer1 = False
+waiting_for_answer2 = False
 
 print("=======================================")
 print("write exit to exit.")
@@ -18,7 +15,7 @@ while True:
     print("                                                ")
 
     if waiting_for_answer:
-        print(random.choice(resp_followup))
+        print(random.choice(resp.followup2))
         print("===========================")
         waiting_for_answer = False
         continue
@@ -31,23 +28,23 @@ while True:
         print["=========================="]
         continue
     elif choice1 in ("good", "im doing good", "im good", "good!", "awesome"):
-        print(random.choice(resp1))
+        print(random.choice(resp.followup1))
         print("==========================")
         continue
     elif choice1 in ("how are you", "how are you doing", "how are you?", "how are you doing?"):
-        print(random.choice(resp2))
+        print(random.choice(resp.question1))
         print("==========================")
         continue
     elif choice1 in ("hello", "hi", "sup", "whats up"):
-        print(random.choice(resp3))
+        print(random.choice(resp.greet))
         print("==========================")
         continue
     elif choice1 == (""):
-        print(random.choice(resp4))
+        print(random.choice(resp.question2))
         print("==========================")
         waiting_for_answer = True
     elif choice1 in ("tell me a joke", "tell a joke", "give me a joke", "joke"):
-        print(random.choice(resp_joke))
+        print(random.choice(resp.joke))
         print("==========================")
         continue
     elif choice1 == "exit":
