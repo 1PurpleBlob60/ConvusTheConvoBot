5c43c20c77d4821ffd69806b633c928ebdcfad54
1PurpleBlob60 <mahe1@skole.trondheim.kommune.no>
2026-09-15 15:24:02 +0000

    1.2

--- BubbleBot/Code.py
+++ BubbleBot/Code.py
@@ -160,9 +160,6 @@ with st.sidebar:
         )
         if st.button("Save username"):
             if new_username.strip():
-                st.session_state.user_preferences[st.session_state.email] = {
-                    "button_color": st.session_state.button_color,
-                }
                 st.session_state.username = new_username.strip()
                 st.session_state.changing_username = False
                 st.rerun()
